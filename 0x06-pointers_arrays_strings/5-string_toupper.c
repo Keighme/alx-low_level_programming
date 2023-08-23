6236667412b14203366c3884c0a19a72ed8b4dfc
keighme <keighmellow@gmail.com>
2023-08-23 14:45:32 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change case
+ * @n: parameter
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int z = 0;
+
+	while (n[z] != '\0')
+	{
+		if (n[z] >= 'a' && n[z] <= 'z')
+		{
+			n[z] = n[z] - 32;
+		}
+		z++;
+	}
+	return (n);
+}
