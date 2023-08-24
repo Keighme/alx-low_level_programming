e39b893870925bdd5a8c4d5e6a2d01a368067e9f
keighme <keighmellow@gmail.com>
2023-08-24 13:40:13 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
-* print_number - print numbers chars
-* @n: integer params
-* Return: 0
-*/
-
+ * print_number - compares two strings.
+ * @n: source string
+ * Return: pointer to the resulting string dest
+ */
 void print_number(int n)
 {
-unsigned int n1;
+	unsigned int x;
 
-n1 = n;
+	x = n;
+	if (n < 0)
+	{
+		x = n * -1;
+		_putchar('-');
+	}
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
-
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+	if (x / 10)
+	{
+		print_number(x / 10);
+		_putchar(x % 10 + '0');
+	}
+	else
+	{
+		_putchar(x + '0');
+	}
 }
