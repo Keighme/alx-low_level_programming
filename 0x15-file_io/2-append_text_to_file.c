5252269fee84bb667ec081f2872e29711378cbe9
keighme <keighmellow@gmail.com>
2023-10-03 13:16:05 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,8 +20,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	}
 
-	o = open(filename, O_WRONGLY | O_APPEND);
-	W = write(o, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
